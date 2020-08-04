62bd09f2adc494413ff08f392d1a69563bd38024
Jeffrey Moulckers <jrmoulckers@gmail.com>
2020-08-04 00:53:08 -0500

    solved leetcode 278 first-bad-version

---
+++ Java/HelperClasses/VersionControl.java
@@ -0,0 +1,8 @@
+package HelperClasses;
+
+// Meaningless placeholder class for Leetcode 278: First Bad Version
+public class VersionControl {
+    public static boolean isBadVersion(int version) {
+        return false;
+    }
+}
---
+++ Java/Leetcode278FirstBadVersion.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+
+import HelperClasses.VersionControl;
+
+/**
+ * Leetcode 278: First Bad Version (EASY)
+ * https://leetcode.com/problems/first-bad-version/
+ * 
+ * You are a product manager and currently leading a team to develop a new product. 
+ * Unfortunately, the latest version of your product fails the quality check. 
+ * Since each version is developed based on the previous version, 
+ * all the versions after a bad version are also bad.
+ * 
+ * Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, 
+ * which causes all the following ones to be bad.
+ * 
+ * You are given an API bool isBadVersion(version) which will return whether version is bad. 
+ * Implement a function to find the first bad version. You should minimize the number of calls to the API.
+ * 
+ * Example:
+ *  Given n = 5, and version = 4 is the first bad version. 
+ *     call isBadVersion(3) -> false
+ *     call isBadVersion(5) -> true
+ *     call isBadVersion(4) -> true
+ *  Then 4 is the first bad version. 
+ */
+public class Leetcode278FirstBadVersion extends VersionControl{
+    // Binary search approach (99.38% spd, 5.19% mem)
+    public static int firstBadVersion(int n) {
+        return getBad(0, n - 1);
+    }
+
+    public static int getBad(int lo, int hi) {
+        if(lo == hi) {
+            return lo;
+        } 
+        int mid = lo + (hi - lo) / 2;
+        
+        // If not bad version, target is beyond this index
+        if(!isBadVersion(mid)) {
+            return getBad(mid + 1, hi);
+        } else {
+            return getBad(lo, mid);
+        }
+    }
+}
