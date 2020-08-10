f7c2960e23f0c05d8737baa6d89f5fabd2bf4598
Jeffrey Moulckers <jrmoulckers@gmail.com>
2020-08-10 01:12:39 -0500

    solved leetcode41 first-missing-positive

--- Java/Leetcode41FirstMissingPositive.java
+++ Java/Leetcode41FirstMissingPositive.java
@@ -21,10 +21,38 @@ import java.util.List;
  */
 public class Leetcode41FirstMissingPositive {
 
+    // Two-pass swap approach (100% spd, 68.60% mem)
     public static int firstMissingPositive(int[] nums) {
-        return -1;
+        int i = 0;
+
+        // Swap values to their (hopefully) corresp. places in the array
+        while(i < nums.length) {
+            // Swap all values that are in the range that can be placed properly in their index
+            // Re-run over this area to check the newly swapped value's correct location
+            if(nums[i] > 0 && nums[i] <= nums.length && nums[i] != nums[nums[i] - 1]) {
+                swap(nums, i, nums[i] - 1);
+            } else {
+                i++;
+            }
+        }
+
+        // Re-run over all, returning the running lowest integer if gap is found
+        for(i = 0; i < nums.length; i++) {
+            if(nums[i] != i + 1) {
+                return i + 1;
+            }        
+        }  
+
+        // There may only be N integers. If all 1 through N match, return N + 1
+        return nums.length + 1;
     }
-    
+
+    private static void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
     public static void main(String[] args) {
         int[][] testInputs = {
             {1, 2, 0},
